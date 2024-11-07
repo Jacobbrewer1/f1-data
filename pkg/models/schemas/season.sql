7ccd5339a4d8e81884f8d2d0223d65e83ed66093
Jacob Brewer <Jacob.brewer@bthree.uk>
2024-11-07 10:40:06 +0000

    debug

--- pkg/models/schemas/season.sql
+++ pkg/models/schemas/season.sql
@@ -1,7 +1,8 @@
 create table season
 (
-    id         int      not null auto_increment,
-    year       int      not null,
+    id       int          not null auto_increment,
+    year     int          not null,
+    template varchar(255) not null,
     primary key (id)
 );
 
